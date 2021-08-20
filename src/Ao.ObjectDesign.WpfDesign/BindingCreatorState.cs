8c19bf848dc89318ea2a68dd5b356407fb9f68be
15218450198@163.com <15218450198@163.com>
2021-08-20 16:25:52 +0800

    add BindingCreatorState

---
+++ src/Ao.ObjectDesign.WpfDesign/BindingCreatorState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Ao.ObjectDesign.WpfDesign
+{
+    public class BindingCreatorState : IBindingCreatorState
+    {
+        public IServiceProvider Provider { get; set; }
+
+        public IDictionary Features { get; set; }
+
+        public DesignRuntimeTypes RuntimeType { get; set; }
+
+        public object GetService(Type serviceType)
+        {
+            var provider = Provider;
+            if (provider is null)
+            {
+                return null;
+            }
+            return provider.GetService(serviceType);
+        }
+    }
+}
