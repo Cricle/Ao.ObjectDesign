a56eae4a1a634c72e0245311ecd52b63d640f239
HCricle <none>
2021-12-12 16:31:32 +0800

    rm some test code

--- samples/ObjectDesign.Wpf/App.xaml.cs
+++ samples/ObjectDesign.Wpf/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using Ao.ObjectDesign.Controls;
-using Ao.ObjectDesign.Designing;
-using Ao.ObjectDesign.Wpf.Xaml;
-using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ObjectDesign.Wpf
 {
@@ -11,27 +7,5 @@ namespace ObjectDesign.Wpf
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            var lst = new List<NotifyableObject>();
-            var btn=new ButtonSetting();
-            btn.SetDefault();
-            lst.Add(btn);
-            var tbx=new TextBlockSetting();
-            tbx.SetDefault();
-            lst.Add(tbx);
-
-            //var str=JsonConvert.SerializeObject(lst,new JsonSerializerSettings { TypeNameHandling= TypeNameHandling.Auto});
-            //var obj= JsonConvert.DeserializeObject<List<UIElementSetting>>(str);
-
-            //var str = DesignMessagePackHelper.Serialize(typeof(List<NotifyableObject>), lst);
-            //var obj = DesignMessagePackHelper.Deserialize(typeof(List<NotifyableObject>), str);
-
-            var str = DeisgnXamlSerializer.Serialize(lst);
-            var obj = DeisgnXamlSerializer.Deserialize(str);
-
-            //var str = DeisgnYamlSerializer.Serialize(lst);
-            //var obj = DeisgnYamlSerializer.Deserializer(str,typeof(List<NotifyableObject>));
-        }
     }
 }
