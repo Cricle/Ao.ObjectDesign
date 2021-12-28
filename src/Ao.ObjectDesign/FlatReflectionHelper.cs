1f7c7743984c81e3cd334a1cd31aa0a8e0f6e608
HCricle <none>
2021-12-28 22:00:41 +0800

    performace

---
+++ src/Ao.ObjectDesign/FlatReflectionHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ao.ObjectDesign
+{
+    public static class FlatReflectionHelper
+    {
+        private static readonly ConcurrentDictionary<Type, IReadOnlyDictionary<string, TypeProperty>> typeToRefs=new ConcurrentDictionary<Type, IReadOnlyDictionary<string, TypeProperty>>();
+        private static IReadOnlyDictionary<string, TypeProperty> GetPropertyMaps(Type type)
+        {
+            return typeToRefs.GetOrAdd(type, CreatePropertyMaps);
+        }
+        private static IReadOnlyDictionary<string, TypeProperty> CreatePropertyMaps(Type type)
+        {
+            return TypeMappings.GetTypeProperties(type).ToDictionary(x => x.Name);
+        }
+
+
+        public static IReadOnlyList<SpecularMappingItem> SpecularMapping(object source, object target)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (target is null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+
+            var sourceProperties = TypeMappings.GetTypeProperties(source.GetType());
+            var destProperties = GetPropertyMaps(target.GetType());
+            var res = new List<SpecularMappingItem>(destProperties.Count);
+            foreach (var item in sourceProperties)
+            {
+                if (item.CanGet &&
+                    destProperties.TryGetValue(item.Name, out var propInfo) &&
+                    propInfo.CanSet)
+                {
+                    Debug.Assert(item.PropertyInfo != null);
+                    Debug.Assert(propInfo.PropertyInfo != null);
+                    if (item.PropertyInfo.PropertyType == propInfo.PropertyInfo.PropertyType)
+                    {
+                        var sourceValue = item.Getter(source);
+                        propInfo.Setter(target, sourceValue);
+                        res.Add(new SpecularMappingItem(item.PropertyInfo, propInfo.PropertyInfo, sourceValue, true));
+                    }
+                    else
+                    {
+                        res.Add(new SpecularMappingItem(item.PropertyInfo, propInfo.PropertyInfo, null, false));
+                    }
+                }
+                else
+                {
+                    res.Add(new SpecularMappingItem(item.PropertyInfo, null, null, false));
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ src/Ao.ObjectDesign/SpecularMappingItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace Ao.ObjectDesign
+{
+    public class SpecularMappingItem
+    {
+        public SpecularMappingItem(PropertyInfo sourceProperty,
+            PropertyInfo targetProperty,
+            object sourceValue,
+            bool succeed)
+        {
+            SourceProperty = sourceProperty;
+            TargetProperty = targetProperty;
+            SourceValue = sourceValue;
+            Succeed = succeed;
+        }
+
+        public PropertyInfo SourceProperty { get; }
+
+        public PropertyInfo TargetProperty { get; }
+
+        public object SourceValue { get; }
+
+        public bool Succeed { get; }
+
+        public override string ToString()
+        {
+            return $"{{PropertyName:{SourceProperty.Name}, Suceed:{Succeed}}}";
+        }
+    }
+}
